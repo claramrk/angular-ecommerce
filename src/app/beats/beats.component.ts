e527ab3489720c05df3449217b6cea14bbbc15e2
Clara Mrkos <mrkos.clara@gmail.com>
2024-01-01 20:18:43 +0100

    Set up pages home about and commit, set up routing and design through tailwind

---
+++ src/app/beats/beats.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beats',
+  standalone: true,
+  imports: [],
+  templateUrl: './beats.component.html',
+  styleUrl: './beats.component.css'
+})
+export class BeatsComponent {
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
